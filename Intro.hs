61cef3f7af734166ce3f7919b1be5133de88b4b3
ola-9 <nepogodda@gmail.com>
2023-08-07 23:37:25 +0300

    init

---
+++ Intro.hs
@@ -0,0 +1,13 @@
+-- Module 1. Intro
+-- 1.1  Basic setup
+
+{-
+    ghci
+    :load ModuleName
+    functionName
+    :realod ModuleName
+    :quit
+-}
+
+module Intro where
+    sayHello = putStrLn "Hello World from module Test! "
